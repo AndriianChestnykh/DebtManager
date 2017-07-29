84f3fd4ce71560e9e844571d18f74ad09f5a91cb
Andriian Chestnykh <achestnykh@provectus.com>
2017-07-29 21:46:40 +0300

    Add stage network

--- truffle.js
+++ truffle.js
@@ -5,5 +5,10 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  stage: {
+    host: "13.93.217.3",
+    port: 22000,
+    network_id: "*" // Match any network id
   }
 };
