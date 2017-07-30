9f5a6ed5d700b4add3d8f8ee8a1948b3cd0dbe17
Demonsh <suhdem@ukr.net>
2017-07-30 10:43:12 +0300

    Ajax request added to the debt list

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var debtList = [[ 2, 3, "0xbbb", 10, true],[4, 5, "0xbbb1111", 20, true]];
+
+  var oList = [[1,"Det","0xtest", true, "0xtet" ],["1", "test","0xed9d02e382b34818e88b88a309c7fe71e65f419d", false, "0xed9d02e382b34818e88b88a309c7fe71e65f419d"]];
+
+
+  res.render('index', { currentDebt: 1000, orderList: oList, debtList: debtList});
 });
 
 module.exports = router;
